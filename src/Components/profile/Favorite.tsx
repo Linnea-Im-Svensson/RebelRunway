871f937a95d00369498b50eae9d3a5c4f93fc49d
Hanna Hosk <hanna.hosk@gmail.com>
2023-10-10 14:49:16 +0200

    added profile page structure

---
+++ src/Components/profile/Favorite.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Favorites = () => {
+  return <div>Favorites section</div>;
+};
+
+export default Favorites;
---
+++ src/pages/profile/profilePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const profilePage = () => {
+  return (
+    <main>
+      <h1>Profile Page</h1>
+    </main>
+  );
+};
+
+export default profilePage;
