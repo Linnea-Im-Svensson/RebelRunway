d43571d830f941fab80ea75ec6da7e70bea9dce7
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2023-10-12 14:38:11 +0200

    mobile

--- src/Components/banner/Banner.tsx
+++ src/Components/banner/Banner.tsx
@@ -1,34 +1,22 @@
 const Banner = () => {
   return (
-    <div className=" flex w-screen justify-center gap-7 bg-white md:w-auto md:justify-around md:gap-20 md:rounded-md md:px-8">
-      <img
-        className=" ml-3 h-12 w-12 md:h-20 md:w-20"
-        src="adidas.jpg"
-        alt="adidas"
-      />
+    <div className=" flex w-screen justify-center gap-2 overflow-hidden rounded-md bg-white px-2 md:w-auto md:justify-around md:gap-20 md:rounded-md md:px-8">
+      <img className="h-12 md:h-20" src="adidas.jpg" alt="adidas" />
 
       <img
-        className=" h-12 w-12 md:h-20 md:w-20 "
+        className="h-12 md:h-20"
         src="the-north-face.jpg"
         alt="The North Face"
       />
-      <img
-        className=" h-12 w-12 md:h-20 md:w-20"
-        src="reebok.jpg"
-        alt="Reebok"
-      />
+      <img className=" h-12 md:h-20" src="reebok.jpg" alt="Reebok" />
 
-      <img className="h-12 w-12 md:h-20 md:w-20" src="nike.jpg" alt="Nike" />
+      <img className="h-12 md:h-20 md:w-20" src="nike.jpg" alt="Nike" />
       <img
-        className="h-12 w-12 md:h-20 md:w-20"
+        className="h-12 md:h-20 "
         src="stone-island.jpg"
         alt="Stone Island"
       />
-      <img
-        className="mr-3 h-12 w-12  md:h-20 md:w-20"
-        src="vans.jpg"
-        alt="Vans"
-      />
+      <img className="h-12 md:h-20 md:w-20" src="vans.jpg" alt="Vans" />
     </div>
   );
 };
