16285110c0fd9137629e600cc2a413293bf423fb
chasacademy-EmmaJohansson <114146126+chasacademy-EmmaJohansson@users.noreply.github.com>
2023-10-13 04:07:31 +0200

    size-button

--- src/components/products/ProductPreview.tsx
+++ src/components/products/ProductPreview.tsx
@@ -1,36 +1,47 @@
+import React from "react";
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import { PiCircleFill } from "react-icons/pi";
+import Dropdown from "./DropDown";
 
-const ProductPreview = ({ color, image, name, price, id }: Product) => {
+interface ProductPreviewProps {
+  product: Product;
+}
+
+export default function ProductPreview({ product }: ProductPreviewProps) {
+  const { color, image, name, price, id } = product;
+
   return (
     <div
       key={id}
       className="flex flex-col items-center gap-1 md:m-2 md:items-start"
     >
       <Image
-        className="h-auto max-w-full rounded-lg"
+        className="h-[360px] w-[270px] rounded-lg"
         src={image}
         alt={name}
         width={270}
         height={360}
       />
       <div className="text-xs font-bold">{name}</div>
-      <div className="text-xs">{price}</div>
+      <div className="text-xs font-semibold">{price} kr</div>
+      <Dropdown products={[product]} productCategory={product.category} />
       <div>
         <div className="mb-1 mt-1 flex gap-2">
           <PiCircleFill
-            className="rounded-xl bg-pink-300 text-sm"
+            className="rounded-xl border border-gray-300 text-sm"
             color={color}
           />
-          <PiCircleFill className="rounded-xl bg-white text-sm" color={color} />
           <PiCircleFill
-            className=" rounded-xl bg-red-700 text-sm"
+            className="rounded-xl border border-gray-300 text-sm"
+            color={color}
+          />
+          <PiCircleFill
+            className="rounded-xl border border-gray-300 text-sm"
             color={color}
           />
         </div>
       </div>
     </div>
   );
-};
-export default ProductPreview;
+}
