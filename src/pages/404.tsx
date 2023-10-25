1d9a874fb1b33e0ecac2400d7aea5ab801ce192e
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2023-10-25 11:25:20 +0200

    ändringar i 404 och successPage

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Link from "next/link";
 
 const notFoundPage = () => {
   return (
-    <div className="mt-5 flex flex-col items-center rounded-lg dark:bg-white">
+    <div className="mt-5 flex flex-col items-center rounded-lg  dark:bg-white">
       <img src="404.png" alt="404" className="rounded-lg" />
       <h1 className="mt-10 font-poppins text-4xl font-bold text-neutral-700 ">
         Oops! Page not found
@@ -11,9 +12,11 @@ const notFoundPage = () => {
         The page you are looking for might have <br /> been removed or
         teporarily unavailable.
       </p>
-      <button className=" m-10 rounded-md border bg-neutral-700 px-5 py-2  text-neutral-100 ">
-        Go back to Homepage
-      </button>
+      <Link href={"/"}>
+        <p className=" mt-10 rounded-md border bg-neutral-700 px-5 py-2  text-neutral-100 ">
+          Go back to Homepage
+        </p>
+      </Link>
     </div>
   );
 };
--- src/pages/successPage.tsx
+++ src/pages/successPage.tsx
@@ -1,8 +1,9 @@
 import { AiOutlineCheck } from "react-icons/ai";
+import Link from "next/link";
 
 function SuccessPage() {
   return (
-    <div className="border-natural-500 mt-10 flex flex-col items-center justify-center rounded-sm  pl-8 pr-8 pt-10 shadow-lg dark:bg-white md:mb-10 md:w-fit md:px-20">
+    <div className="border-natural-500 s mt-16 flex flex-col items-center justify-center rounded-sm border pb-10 pl-8 pr-8 pt-16 shadow-md dark:bg-white md:mb-10 md:w-fit md:px-20">
       <div className=" mb-10 rounded-full bg-green-50 p-10 dark:bg-green-100  md:p-16 ">
         <AiOutlineCheck size={60} className="text-green-500" />
       </div>
@@ -15,12 +16,11 @@ function SuccessPage() {
       <p className="text-natural-800 font-poppins font-bold dark:text-neutral-800 ">
         hejsan svejsan
       </p>
-
-      <button
-        /* onClick={} */ className="mb-10 mt-7 rounded-full  bg-neutral-800 px-5 py-3 font-poppins text-neutral-100"
-      >
+      {/* <Link href={}> */}
+      <p className="mb-10 mt-7 rounded-full  bg-neutral-800 px-5 py-3 font-poppins text-neutral-100">
         Return to cart
-      </button>
+      </p>
+      {/* </Link> */}
     </div>
   );
 }
